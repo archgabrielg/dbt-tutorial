8e1af2b2a9036979a07db7347fa19a8b8c638bde
Gabriel Arch <gabriel.arch@datalytics.com>
2021-11-11 16:53:03 -0300

    Macros

---
+++ models/order_payment_method_amounts.sql
@@ -0,0 +1,12 @@
+{%- set payment_methods = get_column_values(
+    'payment_method', ref('raw_payments')
+) -%}
+
+select
+order_id,
+{%- for payment_method in payment_methods %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount
+{%- if not loop.last %},{% endif -%}
+{% endfor %}
+from {{ ref('raw_payments') }}
+group by 1
